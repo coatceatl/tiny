ba24d5c0098baaa78605c9f5c664fe1acfe1ac66
Oksana Tkachenko <coatceatl@gmail.com>
2020-05-02 22:46:10 +0300

    added mongoose connect

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => {
-  console.log(`server started on ${PORT} port`)
-})
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoURI'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    app.listen(PORT, () => {
+      console.log(`server started on ${PORT} port`)
+    })
+  } catch (e) {
+    console.log('server error', e.message)
+    process.exit(1)
+  }
+}
+
+start()
