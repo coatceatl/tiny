dba6209588f994c7b4d930e48b594fb93b39a558
Oksana Tkachenko <coatceatl@gmail.com>
2020-05-04 12:29:35 +0300

    added scss, changed styles for sign page

--- client/src/pages/SignPage.js
+++ client/src/pages/SignPage.js
@@ -1,45 +1,18 @@
 import React from 'react'
 import { Button, TextField, Grid, Typography, Container } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import { NavLink } from 'react-router-dom'
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  logo: {
-    width: '100px',
-    padding: '50px 0'
-  }
-}));
-
 export const SignPage = () => {
-  const classes = useStyles();
-
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <div className={classes.logo}>
-          <img src='img/logo.svg' />
+    <Container component="main" maxWidth="xs" className="sign">
+      <div className="paper">
+        <div className="logo">
+          <img src="img/logo.svg" />
         </div>
         <Typography component="h1" variant="h5">
           Sign up and start shortening
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className="form">
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -70,7 +43,7 @@ export const SignPage = () => {
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit}
+            className="submit"
           >
             Sign Up
           </Button>
