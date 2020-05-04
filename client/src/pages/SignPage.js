44204d35baa4ebc6af89744aaaa07c4b65ccf591
Oksana Tkachenko <coatceatl@gmail.com>
2020-05-04 12:55:29 +0300

    added handlers for change and submit form data

--- client/src/pages/SignPage.js
+++ client/src/pages/SignPage.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, TextField, Grid, Typography, Container } from '@material-ui/core'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 export const SignPage = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const data = {
+      email: form.email,
+      password: form.password
+    }
+    await axios.post('url', data)
+  }
+
   return (
     <Container component="main" maxWidth="xs" className="sign">
       <div className="paper">
         <div className="logo">
-          <img src="img/logo.svg" />
+          <img src="img/logo.svg" alt="Tiny" />
         </div>
         <Typography component="h1" variant="h5">
           Sign up and start shortening
         </Typography>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -23,6 +43,7 @@ export const SignPage = () => {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -35,6 +56,7 @@ export const SignPage = () => {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                onChange={handleChange}
               />
             </Grid>
           </Grid>
@@ -43,7 +65,7 @@ export const SignPage = () => {
             fullWidth
             variant="contained"
             color="primary"
-            className="submit"
+            className="btn btn-submit"
           >
             Sign Up
           </Button>
